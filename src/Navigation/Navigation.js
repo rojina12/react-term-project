977d9bba43d05aedf35c0d975a6792bb076c26df
Gagan Gill <gagandeepgill94@gmail.com>
2018-11-27 04:07:39 -0500

    Change class to className

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -3,24 +3,24 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#">CPS 530 - Term Project</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">CPS 530 - Term Project</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Summary</a>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Summary</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">How To</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">How To</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Conclusion</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Conclusion</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Credits</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Credits</a>
             </li>
           </ul>
         </div>
