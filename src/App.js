45d555b97c4268f76534791e83cd960c33e7d85c
Gagan Gill <gagandeepgill94@gmail.com>
2018-11-27 04:54:44 -0500

    Restructure folders

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Navigation from './Navigation/Navigation';
-import Summary from './Summary/Summary';
-import Setup from './Setup/Setup';
-import HowTo from './HowTo/HowTo';
-import Demo from './Demo/Demo';
-import Conclusion from './Conclusion/Conclusion';
-import Credits from './Credits/Credits';
+import Summary from './Pages/Summary/Summary';
+import Setup from './Pages/Setup/Setup';
+import HowTo from './Pages/HowTo/HowTo';
+import Demo from './Pages/Demo/Demo';
+import Conclusion from './Pages/Conclusion/Conclusion';
+import Credits from './Pages/Credits/Credits';
 
 class App extends Component {
   render() {
