a22c69728241a7b592e7adf93a9fd49e2517909a
mohamed bobat <mohamed.bobat@hotmail.co.uk>
2018-11-28 21:33:25 -0500

    missing info?

--- src/Pages/HowTo/HowTo.js
+++ src/Pages/HowTo/HowTo.js
@@ -7,6 +7,18 @@ class HowTo extends Component {
       <h1>
         How To
       </h1>
+      <h2>
+      React:
+      </h2>
+      <ol>
+      <li>Install Node.js</li>
+      <li>run 'npm install React' </li>
+      <li>run 'npx create-app *name*'</li>
+      <li>cd to *name*</li>
+      <li>source contains the js code for the app</li>
+      <li>run 'npm start' to start the app.</li>
+      <li>the app will be accesable at http://localhost:3000/</li>
+      </ol>
       </div>
     );
   }
